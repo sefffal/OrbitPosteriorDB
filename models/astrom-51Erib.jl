304ae23fe567451ea6302e44ab4fd43311d383c5
William Thompson <wthompson@uvic.ca>
2024-04-05 14:17:27 -0700

    add 51 eri

--- models/astrom-51Erib.jl
+++ models/astrom-51Erib.jl
@@ -2,17 +2,42 @@ using Octofitter, Distributions
 using CairoMakie
 using PairPlots
 using Pigeons
+using DataFrames
 ##
 
 # Note: will need to download a datadep. Can type `y` or set an ENV variable.
-planet_name = "hd984b"
+planet_name = "51erib"
 gaia_id = 2431157720981843200
-star_mass_unc = 1.16, 0.10
+star_mass_unc = 1.550, 0.005
 # To find the above identifier, search SIMBAD: http://simbad.cds.unistra.fr/simbad/
 # with the name of the system (without the b at the end) and copy the 
 # ID number next to 'GAIA DR2'
-
-astrom_like_seppa, astrom_like_radec = Octofitter.Whereistheplanet_astrom(planet_name)
+astrom_like = PlanetRelAstromLikelihood(DataFrame([
+57009.1  69.3356  -448.917  1.88446  1.81828  -0.00155179
+57052.1  78.3783  -444.96   2.0788   2.05287  -0.00117613
+57053.1  77.8303  -450.121  2.57     2.38622  -0.0209427
+57054.3  76.9638  -455.037  23.8713  24.2003  0.00286876
+57266.4  100.052  -443.966  2.22649  2.07075  -0.0110573
+57290.0  100.8    -442.0    2.9      3.6      0.0
+57291.0  108.7    -440.7    8.8      13.7     0.0
+57332.2  108.641  -439.656  5.39868  4.5087   -0.0172164
+57374.2  112.918  -441.705  6.18322  4.55306  -0.0635376
+57376.2  112.464  -440.892  3.02297  3.39833  0.014624
+57403.0  114.3    -442.2    4.5      5.2      0.0
+57415.0  110.406  -440.845  6.03121  4.07169  -0.0682188
+57649.4  142.053  -432.057  2.01957  2.05944  0.00370176
+57652.4  141.521  -428.673  2.66361  2.44022  -0.00564645
+57734.0  152.9    -427.1    3.4      4.6      0.0
+57739.1  153.258  -422.449  2.15789  2.1202   -0.00251691
+58024.0  185.0    -409.2    2.0      2.1      0.0
+58068.3  187.509  -406.365  3.01691  3.05686  -0.00638734
+58442.2  219.468  -374.674  2.00066  1.76217  0.0759851
+58379.0  220.2    -384.8    2.8      3.1      0.0
+], [
+   :epoch,    :ra, :dec, :σ_ra, :σ_dec, :cor
+]))
+gaia_id = 3205095125321700480
+hgcal   = HGCALikelihood(;gaia_id)
 
 # To inspect data: astrom_like.table
 
@@ -20,10 +45,10 @@ astrom_like_seppa, astrom_like_radec = Octofitter.Whereistheplanet_astrom(planet
 # astrom_like = PlanetRelAstromLikelihood(astrom_like_0.table[1:end,1,1])
 
 # "gold standard" results
-chain_orbitize = Octofitter.loadhdf5(planet_name)
+# chain_orbitize = Octofitter.loadhdf5(planet_name)
 
 # Set angle reference epoch to average date of observations
-const t_ref = mean(astrom_like_radec.table.epoch)
+const t_ref_epoch = mean(astrom_like.table.epoch)
 
 
 
@@ -41,8 +66,8 @@ const t_ref = mean(astrom_like_radec.table.epoch)
     ω ~ UniformCircular()
     Ω ~ UniformCircular()
     θ ~ UniformCircular()
-    tp = θ_at_epoch_to_tperi(system,b,t_ref) 
-end astrom_like_radec astrom_like_seppa
+    tp = θ_at_epoch_to_tperi(system,b,t_ref_epoch) 
+end astrom_like
 @system system_param1 begin
     plx ~ gaia_plx(;gaia_id)
     M   ~ truncated(Normal(star_mass_unc...), lower=0)
@@ -58,8 +83,8 @@ model_param1 = Octofitter.LogDensityModel(system_param1)
     ω ~ Uniform(0,2pi)
     Ω ~ Uniform(0,2pi)
     θ ~ Uniform(0,2pi)
-    tp = θ_at_epoch_to_tperi(system,b,t_ref) 
-end astrom_like_radec astrom_like_seppa
+    tp = θ_at_epoch_to_tperi(system,b,t_ref_epoch) 
+end astrom_like
 @system system_param2 begin
     plx ~ gaia_plx(;gaia_id)
     M   ~ truncated(Normal(star_mass_unc...), lower=0)
@@ -74,10 +99,10 @@ pt = pigeons(
     target=model_param2,
     n_rounds=10,
     n_chains=24,
-    explorer = #Compose(
+    explorer = Compose(
         SliceSampler(p=10),
-        #AutoMALA(),
-    # ),
+        AutoMALA(),
+    ),
     record = [traces; round_trip; record_default();  record_online()],
     multithreaded=true,
     show_report=true,
@@ -89,8 +114,8 @@ chain_pt = Chains(pt.inputs.target, pt)
 octocorner(
     model_param2,
     # Compare three different samplers:
-    chain_orbitize, 
-    # chain_nuts,
+    # chain_orbitize, 
+    chain_nuts,
     chain_pt,
 
     viz=(
